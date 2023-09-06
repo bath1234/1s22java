56b0ac45a050ff396098dd02fcd199c3bffdfc8b
bath1234 <144190850+bath1234@users.noreply.github.com>
2023-09-05 19:48:32 -0600

    practica 1 y 2 del 5 de septiembre del 2023

---
+++ p1_1s22.java
@@ -0,0 +1,9 @@
+class p1_1s22{
+	public static void main(String[] args){
+		System.out.println("Brayan Afdel Tolentino Hermegildo");
+		System.out.println("20 años");
+		System.out.println("elbrayangm2@gmail.com");
+		System.out.println("Me gustan los videojuegos");
+		System.out.println("Mi color favorito es el verde");
+	}
+}
